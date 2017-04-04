a571ec6c2b7a0cdd1e4d5557330ef33d36a391c4
Vishal Kaul <vishal.kaul@autodesk.com>
2017-04-05 01:34:46 +0800

    Pokemon Progress

--- src/app.js
+++ src/app.js
@@ -1,21 +1,32 @@
 var pokemonApp = angular.module("pokemon", ['ui.grid']);
-pokemonApp.controller('pokemonController', function ($scope, $http, $q) {
+pokemonApp.controller('pokemonController', function ($scope, $http, $q, $filter) {
 
 	var that = this;
+	that.showDetails = false;
 	
 	that.gridOptions = {
+		enableFiltering: true
+		};
+
+	that.filterOptions = {
+			filterText: ''
 		};
 
 	this.gridOptions.columnDefs = [
 		{
-			name:'ename'
+			name:'ename',
+			filter: {
+					term: this.filterOptions.filterText //DOES NOT WORK... BUT WHY
+				}
 		},
 		{
 			name:'id'
 		},
 		{
 			name: 'theme',
-			'cellTemplate': 'image'
+			'cellTemplate': 'image',
+			enableFiltering: false,
+			enableSorting: false
 		}
 	];
 
@@ -26,9 +37,21 @@ pokemonApp.controller('pokemonController', function ($scope, $http, $q) {
 			console.log(data)
 			that.gridOptions.data = data.data;
 			console.log(that.gridOptions.data);
+			for (var i = 0, len = that.gridOptions.data.length; i < len; i++) {
+			  console.log(that.gridOptions.data[i]);
+			  that.gridOptions.data[i].showDetails = false;
+			}
 		});
 
 		$scope.$on('$destroy', function(){
 			canceler.resolve();  // Aborts the $http request if it isn't finished.
 	 });
+
+	that.expand = function (obj) {
+		if (obj.showDetails === true) {
+			obj.showDetails = false;
+		} else {
+			obj.showDetails = true;
+		}
+	}
 });
