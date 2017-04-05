d239208f6ddb6c428b5f43a628b5c7841b04b822
Vishal Kaul <vishal.kaul@autodesk.com>
2017-04-06 00:35:38 +0800

    Documentation Added

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ Steps to run:
 2) Do 'npm install'
 3) Do 'grunt'
 4) Open any web browser and goto http://localhost:8000 to see the application
+
+Steps To Check Scenarios:
+
+1) Click on the arrows on the column header to sort ASC or DESC
+2) Type the ename or the id in the correponding input boxes at the column to do filtering
+3) Either click on the pokemon or the 'Show Details' link to show the details of the pokemon. The same can be hidden via clicking the 'Hide Details' link. These 2 links toggle.
+4) It's responsive..
--- src/app.js
+++ src/app.js
@@ -1,23 +1,40 @@
 var pokemonApp = angular.module("pokemon", ['ui.grid']);
 pokemonApp.controller('pokemonController', function ($scope, $http, $q, $filter) {
 
-	var that = this;
+	var that = this; // Preserving the this for the controller
+
+	/**
+	 * @ngdoc property
+	 * @name showDetails
+	 * @description This is the boolean for managing the toggling of the details section
+	 */
 	that.showDetails = false;
 	
+	/**
+	 * @ngdoc property
+	 * @name gridOptions
+	 * @description This is the grid options for the grid. It will be populated with data
+	 */
 	that.gridOptions = {
 		enableFiltering: true
-		};
+	};
 
+	/**
+	 * @ngdoc property
+	 * @name filterOptions
+	 * @description It's the default filterOptions for filtering the columns
+	 */
 	that.filterOptions = {
 			filterText: ''
-		};
+	};
 
+	// Setting up the options and the filter configuration
 	this.gridOptions.columnDefs = [
 		{
 			name:'ename',
 			filter: {
-					term: this.filterOptions.filterText //DOES NOT WORK... BUT WHY
-				}
+				term: this.filterOptions.filterText
+			}
 		},
 		{
 			name:'id'
@@ -32,21 +49,27 @@ pokemonApp.controller('pokemonController', function ($scope, $http, $q, $filter)
 
 	var canceler = $q.defer();
 
+	// Getting the pokemons data
 	$http.get('data/pokedex.json', {timeout: canceler.promise}).then(
 		function(data) {
-			console.log(data)
-			that.gridOptions.data = data.data;
-			console.log(that.gridOptions.data);
+			that.gridOptions.data = data.data; // Assigning the data so as to render
 			for (var i = 0, len = that.gridOptions.data.length; i < len; i++) {
-			  console.log(that.gridOptions.data[i]);
-			  that.gridOptions.data[i].showDetails = false;
+				// Adding showDetails property to the object to manage showing details
+				that.gridOptions.data[i].showDetails = false;
 			}
 		});
 
-		$scope.$on('$destroy', function(){
-			canceler.resolve();  // Aborts the $http request if it isn't finished.
-	 });
+	$scope.$on('$destroy', function(){
+		canceler.resolve();  // Aborts the $http request if it isn't finished.
+	});
 
+	/**
+	 * @ngdoc method
+	 * @name expand
+	 * @description Manages the expand and collapse of the details section in the theme column
+	 *
+	 * @param {Object} obj The row object
+	 */
 	that.expand = function (obj) {
 		if (obj.showDetails === true) {
 			obj.showDetails = false;
